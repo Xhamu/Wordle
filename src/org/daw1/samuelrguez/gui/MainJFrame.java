937a6af2b77bbcd715bc1b3c50340e066fb0f63e
Xhamu <samuelgeminis5@gmail.com>
2022-05-05 09:48:17 +0200

    Creación de proyecto y paquete para interfaz

--- src/org/daw1/samuelrguez/gui/MainJFrame.java
+++ src/org/daw1/samuelrguez/gui/MainJFrame.java
@@ -9,12 +9,12 @@ package org.daw1.samuelrguez.gui;
  *
  * @author alumno
  */
-public class InterfazWordle extends javax.swing.JFrame {
+public class MainJFrame extends javax.swing.JFrame {
 
     /**
      * Creates new form InterfazWordle
      */
-    public InterfazWordle() {
+    public MainJFrame() {
         initComponents();
     }
 
@@ -60,20 +60,21 @@ public class InterfazWordle extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(InterfazWordle.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(MainJFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(InterfazWordle.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(MainJFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(InterfazWordle.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(MainJFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(InterfazWordle.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(MainJFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
+        //</editor-fold>
         //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new InterfazWordle().setVisible(true);
+                new MainJFrame().setVisible(true);
             }
         });
     }
